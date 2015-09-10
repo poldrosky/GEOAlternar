718d69bb44f447e82307d82ac5bff3133d40c250
edixred <edixred@edixred>
2015-09-09 23:32:39 -0500

    bayron 9 sep- pruebas oplenlayer3

--- web/resources/js/googlemapsRiver.js
+++ web/resources/js/googlemapsRiver.js
@@ -1,35 +1,18 @@
-var map,markers,marker;
-var gml,urlpath;
-var arrMarkers = [];
-window.onload = function () {
-var layers = [
-  new google.maps.Map(document.getElementById('map'), {
-      disableDefaultUI: true,
-      keyboardShortcuts: false,
-      draggable: false,
-      disableDoubleClickZoom: true,
-      scrollwheel: false,
-      streetViewControl: false
-    }),
-  new ol.layer.Image({
-        source: new ol.source.ImageWMS({
-          ratio: 1,
-          url: 'http://geoalternar.udenar.edu.co:8080/geoserver/Hidraulica/wms',
-          params: {'FORMAT': 'image/png',
-                   'VERSION': '1.1.1',  
-                STYLES: '',
-                LAYERS: 'Hidraulica:Drenaje_Sencillo',
-          }
-        })
-      })
-];
-var map = new ol.Map({
-  layers: layers,
-  target: 'map',
-  view: new ol.View({
-    center: [-78.028, 1.409],
-    zoom: 4
-  })
-});
 
-}
+var map = new ol.Map({ 
+   layers: [ 
+     new ol.layer.Tile({ 
+        source: new ol.source.OSM() 
+     }) 
+   ], 
+   renderer: 'canvas',
+   target: 'map', 
+   view: new ol.View({ 
+     center: [0, 0], 
+     zoom: 2 
+   }) 
+}); 
+
+//Añadimos un control de zoom 
+
+map.addControl(new ol.control.ZoomSlider()); 
